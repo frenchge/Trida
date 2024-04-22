4eb0ad4558b0e850b26e9df28ae9e44729dc41ff
frenchge <lahmerselim@gmail.com>
2024-04-22 06:05:11 +0200

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,68 @@
+import type { Config } from 'tailwindcss';
+
+const config = {
+  darkMode: ['class'],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  prefix: '',
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    extend: {
+      colors: {
+        dark: {
+          1: '#283328',
+          2: '#161c16',
+          3: '#244024',
+          4: '#1e5222',
+        },
+        blue: {
+          1: '#899c89',
+        },
+        sky: {
+          1: '#caffc7',
+          2: '#dff2df',
+          3: '#f5fcf2',
+        },
+        orange: {
+          1: '#FF742E',
+        },
+        purple: {
+          1: '#830EF9',
+        },
+        yellow: {
+          1: '#F9A90E',
+        },
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+      backgroundImage: {
+        hero: "url('/images/hero-background.png')",
+      },
+    },
+  },
+  plugins: [require('tailwindcss-animate')],
+} satisfies Config;
+
+export default config;
